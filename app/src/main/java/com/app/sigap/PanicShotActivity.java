4cb800eb25bb79b64cfed2f7525d085a2286aecf
Parama Dharmika <parama.dharmika@gmail.com>
2016-11-25 04:26:09 +0700

    Showing file upload progress using progress dialog, when sending panic request

--- app/src/main/java/com/app/sigap/PanicShotActivity.java
+++ app/src/main/java/com/app/sigap/PanicShotActivity.java
@@ -2,6 +2,7 @@ package com.app.sigap;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.ProgressDialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -11,6 +12,7 @@ import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Typeface;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
@@ -19,7 +21,6 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.view.Window;
@@ -31,22 +32,25 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.app.master.MainMenuActivity;
+import com.app.request.AndroidMultipartEntity;
 import com.app.sources.MainMenuIDE;
 import com.app.sources.PanikLog;
 import com.app.sources.SQLConnection;
 
-import java.io.ByteArrayOutputStream;
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.mime.content.FileBody;
+import org.apache.http.entity.mime.content.StringBody;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
 import java.io.File;
+import java.io.IOException;
 import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Map;
 
 public class PanicShotActivity extends AppCompatActivity {
     private static final int CAMERA_AND_STORAGE_PERMISSION = 1945;
@@ -60,9 +64,12 @@ public class PanicShotActivity extends AppCompatActivity {
 
     private double latitude;
     private double longitude;
-    private Bitmap myBitmap;
     private Uri uriFilePath;
     private String imgTakenPath;
+
+    private ProgressDialog progressDialog;
+    private long totalSize = 0;
+    private String imageName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -190,6 +197,11 @@ public class PanicShotActivity extends AppCompatActivity {
     }
 
     private void sendRequest() {
+        progressDialog = new ProgressDialog(this);
+
+//        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setIndeterminate(true);
+
         btnSend = (Button) findViewById(R.id.btnSend);
         txtDescription = (EditText) findViewById(R.id.txtDescription);
 
@@ -235,13 +247,13 @@ public class PanicShotActivity extends AppCompatActivity {
         "image      : " + VAR_IMAGE + "\n" +
         "keterangan : " + VAR_KETERANGAN;
 
-        System.out.println(message);
+//        System.out.println(message);
+//        message = "Foto sedang diupload." + "\n" + "Harap tunggu sampai proses selesai.";
+//        Toast.makeText(PanicShotActivity.this, message, Toast.LENGTH_LONG).show();
 
-        message = "Foto sedang diupload." + "\n" + "Harap tunggu sampai proses selesai.";
-        Toast.makeText(PanicShotActivity.this, message, Toast.LENGTH_LONG).show();
+//        txtDescription.setVisibility(View.GONE);
+//        btnSend.setVisibility(View.GONE);
 
-        txtDescription.setVisibility(View.GONE);
-        btnSend.setVisibility(View.GONE);
         /**
          * Prepared post image
          * */
@@ -293,7 +305,8 @@ public class PanicShotActivity extends AppCompatActivity {
         if (packageManager.hasSystemFeature(PackageManager.FEATURE_CAMERA)) {
             Calendar calendar = Calendar.getInstance();
 
-            File file = new File(Environment.getExternalStorageDirectory(), "IMG_" + calendar.getTimeInMillis() + ".jpg");
+            imageName = "IMG_" + calendar.getTimeInMillis() + ".jpg";
+            File file = new File(Environment.getExternalStorageDirectory(), imageName);
 
             if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
                 uriFilePath = FileProvider.getUriForFile(this, getPackageName() + ".provider", file);
@@ -335,6 +348,10 @@ public class PanicShotActivity extends AppCompatActivity {
                     bitmap = android.provider.MediaStore.Images.Media
                             .getBitmap(cr, selectedImage);
 
+                    // store the image so we can send this file later to our server
+                    MediaStore.Images.Media.insertImage(cr, bitmap, imageName, "panic image");
+
+                    // display bitmap to view
                     imgPanicSituation.setImageBitmap(bitmap);
                 } catch (Exception e) {
                     Toast.makeText(this, "Try again please.", Toast.LENGTH_SHORT).show();
@@ -347,95 +364,116 @@ public class PanicShotActivity extends AppCompatActivity {
         }
     }
 
-    private String getStringImage ()
-    {
-        ContentResolver cr = getContentResolver();
-        Bitmap bitmap;
-
-        try {
-            bitmap = android.provider.MediaStore.Images.Media
-                    .getBitmap(cr, uriFilePath);
-
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-
-            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
-
-            byte[] imageBytes = baos.toByteArray();
-
-            String encodeImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
-
-            return encodeImage;
-        } catch (Exception e) {
-            return "";
-        }
-    }
+//    private String getStringImage ()
+//    {
+//        ContentResolver cr = getContentResolver();
+//        Bitmap bitmap;
+//
+//        try {
+//            bitmap = android.provider.MediaStore.Images.Media
+//                    .getBitmap(cr, uriFilePath);
+//
+//            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+//
+//            bitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+//
+//            byte[] imageBytes = baos.toByteArray();
+//
+//            String encodeImage = Base64.encodeToString(imageBytes, Base64.DEFAULT);
+//
+//            return encodeImage;
+//        } catch (Exception e) {
+//            return "";
+//        }
+//    }
 
     private void Save ()
     {
         /**
          * Set request method
          * */
-        StringRequest stringRequest;
-        stringRequest = new StringRequest
-                (
-                    Request.Method.POST,
-                    SQLConnection.URL_PANIC,
-                    new Response.Listener<String>()
-                    {
-                        @Override
-                        public void onResponse(String response)
-                        {
-                            String message;
+//        StringRequest stringRequest;
+//        stringRequest = new StringRequest
+//                (
+//                    Request.Method.POST,
+//                    SQLConnection.URL_PANIC,
+//                    new Response.Listener<String>()
+//                    {
+//                        @Override
+//                        public void onResponse(String response)
+//                        {
+//                            String message;
+//
+//                            /**
+//                             * Jika respon gagal
+//                             * */
+//                            if(!response.equalsIgnoreCase(SQLConnection.PANIC_SUCCESS))
+//                            {
+//                                //  gagal
+//                            }
+//                            else
+//                            {
+//                                /**
+//                                 * Success
+//                                 * */
+//                                setMessage();
+//                            }
+//                        }
+//                    },
+//                    new Response.ErrorListener()
+//                    {
+//                        @Override
+//                        public void onErrorResponse(VolleyError error)
+//                        {
+//                            /**
+//                             * Tambahkan apa yang terjadi setelah Pesan Error muncul, alternatif
+//                             * */
+//                            //setMessage(error.getMessage());
+//                        }
+//                    }
+//                ){
+//            protected Map<String, String> getParams()
+//            {
+//                /**
+//                 * Set parameter to send database
+//                 * */
+//                Map<String,String> params = new HashMap<String, String>();
+//                params.put(SQLConnection.KEY_USERNAME, PanikLog.getUsername());
+//                params.put(SQLConnection.KEY_LATITUDE, PanikLog.getLatitude());
+//                params.put(SQLConnection.KEY_LONGITUDE, PanikLog.getLongitude());
+//                params.put(SQLConnection.KEY_IMAGE, getStringImage());
+//                params.put(SQLConnection.KEY_KETERANGAN, PanikLog.getKeterangan());
+//                return params;
+//            }
+//        };
 
-                            /**
-                             * Jika respon gagal
-                             * */
-                            if(!response.equalsIgnoreCase(SQLConnection.PANIC_SUCCESS))
-                            {
-                                //  gagal
-                            }
-                            else
-                            {
-                                /**
-                                 * Success
-                                 * */
-                                setMessage();
-                            }
-                        }
-                    },
-                    new Response.ErrorListener()
-                    {
-                        @Override
-                        public void onErrorResponse(VolleyError error)
-                        {
-                            /**
-                             * Tambahkan apa yang terjadi setelah Pesan Error muncul, alternatif
-                             * */
-                            //setMessage(error.getMessage());
-                        }
-                    }
-                ){
-            protected Map<String, String> getParams()
-            {
-                /**
-                 * Set parameter to send database
-                 * */
-                Map<String,String> params = new HashMap<String, String>();
-                params.put(SQLConnection.KEY_USERNAME, PanikLog.getUsername());
-                params.put(SQLConnection.KEY_LATITUDE, PanikLog.getLatitude());
-                params.put(SQLConnection.KEY_LONGITUDE, PanikLog.getLongitude());
-                params.put(SQLConnection.KEY_IMAGE, getStringImage());
-                params.put(SQLConnection.KEY_KETERANGAN, PanikLog.getKeterangan());
-                return params;
-            }
-        };
+//        MultipartRequest panicRequest = new MultipartRequest(SQLConnection.URL_PANIC,
+//                new Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//
+//                    }
+//                }, new Response.Listener<String>() {
+//                    @Override
+//                    public void onResponse(String response) {
+//
+//                    }
+//                }, null, "");
+//
+//        /**
+//         * Set volley request
+//         * */
+//        RequestQueue requestQueue = Volley.newRequestQueue(this);
+//
+//        requestQueue.add(panicRequest);
 
-        /**
-         * Set volley request
-         * */
-        RequestQueue requestQueue;
-        requestQueue = Volley.newRequestQueue(this);
-        requestQueue.add(stringRequest);
+        new UploadFileToServer(
+                SQLConnection.URL_PANIC,
+                PanikLog.getUsername(),
+                PanikLog.getLatitude(),
+                PanikLog.getLongitude(),
+                PanikLog.getKeterangan())
+            .execute();
     }
 
     @SuppressWarnings("")
@@ -512,4 +550,114 @@ public class PanicShotActivity extends AppCompatActivity {
         alertDialog.show();
     }
 
+    private class UploadFileToServer extends AsyncTask<Void, Integer, String> {
+        private String url;
+        private final String currentUserName;
+        private final String latitude;
+        private final String longitude;
+        private final String keterangan;
+
+        public UploadFileToServer(String url, String currentUserName,
+                                  String latitude, String longitude, String keterangan) {
+            this.url = url;
+            this.currentUserName = currentUserName;
+            this.latitude = latitude;
+            this.longitude = longitude;
+            this.keterangan = keterangan;
+        }
+
+        @Override
+        protected void onPreExecute() {
+            // setting progress bar to zero
+            progressDialog.setProgress(0);
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progress) {
+            // Making progress bar visible
+            progressDialog.show();
+
+            // updating progress bar value
+            progressDialog.setProgress(progress[0]);
+            progressDialog.setMessage(String.format("Uploading %d", progress[0]));
+
+            Log.d("SigapUploadProgress", String.valueOf(progress[0]) + "%");
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+            return uploadFile();
+        }
+
+        @SuppressWarnings("deprecation")
+        private String uploadFile() {
+            String responseString = null;
+
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httppost = new HttpPost(url);
+
+            try {
+                AndroidMultipartEntity entity = new AndroidMultipartEntity(
+                        new AndroidMultipartEntity.ProgressListener() {
+
+                            @Override
+                            public void transferred(long num) {
+                                publishProgress((int) ((num / (float) totalSize) * 100));
+                            }
+                        });
+
+                // Adding file data to http body
+                File sourceFile = new File(Environment.getExternalStorageDirectory()
+                        .getAbsolutePath(), imageName);
+
+                // Adding file data to http body
+                entity.addPart("image", new FileBody(sourceFile));
+
+                entity.addPart("username", new StringBody(this.currentUserName));
+                entity.addPart("latitude", new StringBody(this.latitude));
+                entity.addPart("longitude", new StringBody(this.longitude));
+                entity.addPart("keterangan", new StringBody(this.keterangan));
+
+                totalSize = entity.getContentLength();
+                httppost.setEntity(entity);
+
+                // Making server call
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity r_entity = response.getEntity();
+
+                int statusCode = response.getStatusLine().getStatusCode();
+                if (statusCode == 200) {
+                    // Server response
+                    responseString = EntityUtils.toString(r_entity);
+                } else {
+                    responseString = "Error occurred! Http Status Code: " + statusCode;
+                }
+
+            } catch (ClientProtocolException e) {
+                responseString = e.toString();
+            } catch (IOException e) {
+                responseString = e.toString();
+            }
+
+            return responseString;
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            progressDialog.dismiss();
+
+            if (!result.equalsIgnoreCase(SQLConnection.PANIC_SUCCESS)) {
+                // todo: fill this condition
+            } else {
+                // todo: fill this condition
+            }
+
+            setMessage();
+        }
+    }
 }
